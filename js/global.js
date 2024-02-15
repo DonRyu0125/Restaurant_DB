e1f8d4666e3cedd95adb6e3805a62328afe19c65
DonRyu0125 <don@minisisinc.com>
2024-02-15 11:13:20 -0800

    pagination css

--- js/global.js
+++ js/global.js
@@ -1,17 +1,17 @@
-$(".bookmark-btn").on("click", function() {
-    console.log("clicked")
-    // Create a form dynamically
-    var form_string = '<form method="post" action="' + $(".web_sum_form").attr("data-action") + '"></form>';
-    var myform = $(form_string);
-    var input_field = '<input type="text" name="' + $(this).find("input").attr("name") + '" value="' + $(this).find("input").attr("value") + '">';
-    console.log('input==>',input_field);
-    myform.append(input_field);
-    console.log('form==>',myform)
-    console.log('this',this)
-    $('body').append(myform);
-    // sumbit form
-    $(myform).submit();
-    /*
+$(".bookmark-btn").on("click", function () {
+  console.log("clicked");
+  // Create a form dynamically
+  var form_string = '<form method="post" action="' + $(".web_sum_form").attr("data-action") + '"></form>';
+  var myform = $(form_string);
+  var input_field = '<input type="text" name="' + $(this).find("input").attr("name") + '" value="' + $(this).find("input").attr("value") + '">';
+  console.log("input==>", input_field);
+  myform.append(input_field);
+  console.log("form==>", myform);
+  console.log("this", this);
+  $("body").append(myform);
+  // sumbit form
+  $(myform).submit();
+  /*
       if (count == false) {
           count = true;
           // alert($(".web_sum_form").attr('data-action'))
@@ -37,18 +37,24 @@ $(".bookmark-btn").on("click", function() {
       }
     */
 });
-$("#web_sum_form").on("submit", function(e) {
-    e.preventDefault();
+$("#web_sum_form").on("submit", function (e) {
+  e.preventDefault();
 });
 
-$(".Delete-Bookmark").on("click", function() {
-    $.ajax({
-        url: $("#web_sum_form").attr("action"),
-        type: "POST",
-        data: $(this).find("input").attr("name") +
-            "=" +
-            $(this).find("input").attr("value"),
-    }).then(function(data) {
-        location.reload();
-    });
+$(".Delete-Bookmark").on("click", function () {
+  $.ajax({
+    url: $("#web_sum_form").attr("action"),
+    type: "POST",
+    data: $(this).find("input").attr("name") + "=" + $(this).find("input").attr("value"),
+  }).then(function (data) {
+    location.reload();
+  });
 });
+
+function goBack() {
+  window.history.back();
+}
+
+function goForward() {
+  window.history.forward();
+}
